5107428c74a7e17d7ba868f8f8e2b6d95c881de3
Jarx246 <51779056+Jarx246@users.noreply.github.com>
2025-12-25 02:16:57 +0800

    refactor: use solid-styled-jsx

--- src/app.tsx
+++ src/app.tsx
@@ -2,22 +2,70 @@ import { MetaProvider, Title } from "@solidjs/meta";
 import { A, Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
-import "./app.css";
+import type {} from "solid-styled-jsx";
+// import "./app.css";
 
 export default function App() {
   return (
-    <Router
-      base={import.meta.env.SERVER_BASE_URL} // This pulls from app.config.ts
-      root={(props) => (
-        <MetaProvider>
-          <Title>SolidStart - Basic</Title>
-          <A href="/">Index</A>
-          <A href="about">About</A>
-          <Suspense>{props.children}</Suspense>
-        </MetaProvider>
-      )}
-    >
-      <FileRoutes />
-    </Router>
+    <>
+      <Router
+        base={import.meta.env.SERVER_BASE_URL} // This pulls from app.config.ts
+        root={(props) => (
+          <MetaProvider>
+            <Title>SolidStart - Basic</Title>
+            <A href="/">Index</A>
+            <A href="about">About</A>
+            <Suspense>{props.children}</Suspense>
+          </MetaProvider>
+        )}
+      >
+        <FileRoutes />
+      </Router>
+      <style jsx global>
+        {`
+          body {
+            font-family:
+              Gordita, Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+              sans-serif;
+          }
+
+          a {
+            margin-right: 1rem;
+          }
+
+          main {
+            text-align: center;
+            padding: 1em;
+            margin: 0 auto;
+          }
+
+          h1 {
+            color: #335d92;
+            text-transform: uppercase;
+            font-size: 4rem;
+            font-weight: 100;
+            line-height: 1.1;
+            margin: 4rem auto;
+            max-width: 14rem;
+          }
+
+          p {
+            max-width: 14rem;
+            margin: 2rem auto;
+            line-height: 1.35;
+          }
+
+          @media (min-width: 480px) {
+            h1 {
+              max-width: none;
+            }
+
+            p {
+              max-width: none;
+            }
+          }
+          `}
+      </style>
+    </>
   );
 }
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,15 +1,42 @@
 import { createSignal } from "solid-js";
-import "./Counter.css";
+// import "./Counter.css";
 
 export default function Counter() {
   const [count, setCount] = createSignal(0);
   return (
-    <button
-      class="increment"
-      onClick={() => setCount(count() + 1)}
-      type="button"
-    >
-      Clicks: {count()}
-    </button>
+    <>
+      <button
+        class="increment"
+        onClick={() => setCount(count() + 1)}
+        type="button"
+      >
+        Clicks: {count()}
+      </button>
+      <style jsx dynamic>
+        {`
+          .increment {
+            font-family: inherit;
+            font-size: inherit;
+            padding: 1em 2em;
+            color: #335d92;
+            background-color: rgba(68, 107, 158, 0.1);
+            border-radius: 2em;
+            border: 2px solid rgba(68, 107, 158, 0);
+            outline: none;
+            width: 200px;
+            font-variant-numeric: tabular-nums;
+            cursor: pointer;
+          }
+
+          .increment:focus {
+            border: 2px solid #335d92;
+          }
+
+          .increment:active {
+            background-color: rgba(68, 107, 158, 0.2);
+          }
+        `}
+      </style>
+    </>
   );
 }
